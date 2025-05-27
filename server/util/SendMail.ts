4de7b53a5d74f78e85a187b1226050ada94a7799
Mangesh dev <deomangesh06@gmail.com>
2025-05-27 14:54:32 +0530

    initial commit

--- server/util/SendMail.ts
+++ server/util/SendMail.ts
@@ -20,7 +20,7 @@ const Transporter = nodemailer.createTransport({
 const otp = generateOtp();
 
 export async function SendMail(to: string) {
-  const templatePath = path.join(process.cwd(), "src/email/email.html");
+  const templatePath = path.join(process.cwd(), "/email/email.html");
   let emailTemplate = fs.readFileSync(templatePath, "utf-8");
 
   emailTemplate = emailTemplate.replace("{{code}}", otp);
